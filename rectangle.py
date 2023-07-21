5a378964f07fddbfba27944f94a0dbf183449148
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2023-07-21 14:09:41 +0430

    feat(rectangle): init get, set interfaces

--- rectangle.py
+++ rectangle.py
@@ -3,14 +3,32 @@ LENGTH_TYPE = float
 
 class Rectangle:
     def __init__(self, width: LENGTH_TYPE, height: LENGTH_TYPE):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @staticmethod
-    def __validate_length__(length: LENGTH_TYPE) -> bool:
+    def __validate_length(length: LENGTH_TYPE) -> bool:
         return length > 0
 
     def compute_area(self) -> LENGTH_TYPE:
-        if self.__validate_length__(self.width) and self.__validate_length__(self.height):
-            return self.width * self.height
+        if self.__validate_length(self.__width) and self.__validate_length(
+            self.__height
+        ):
+            return self.__width * self.__height
         return 0
+
+    @property
+    def width(self):
+        pass
+
+    @width.setter
+    def width(self, value: LENGTH_TYPE):
+        pass
+
+    @property
+    def height(self):
+        pass
+
+    @height.setter
+    def height(self, value: LENGTH_TYPE):
+        pass
